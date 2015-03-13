276bbd1057868ce317b88d88799a3306fd86f44c
Susan <suzanmeryam@gmail.com>
2015-03-13 15:36:39 -0700

    first rspec commit

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,26 +1,42 @@
 require 'rails_helper'
+require 'spec_helper'
 
 RSpec.describe UsersController, type: :controller do
 
-  describe "GET #index" do
-    it "returns http success" do
+
+
+
+ describe "GET #index" do
+    before(:each) do
       get :index
-      expect(response).to have_http_status(:success)
+    end
+
+    it "should have status code of 200" do
+      expect(response).to have_http_status(200)
+    end
+
+    it 'should render the correct page' do
+      expect(response).to render_template :index
     end
   end
 
-  describe "GET #show" do
-    it "returns http success" do
-      get :show
-      expect(response).to have_http_status(:success)
-    end
-  end
 
-  describe "GET #new" do
-    it "returns http success" do
-      get :new
-      expect(response).to have_http_status(:success)
+  describe "DELETE #destroy" do
+    before(:each) do
+      @user1 = User.create!(first_name:'Susan', last_name:'Mansoori', email:'example1@gmail.com', password: 'meow123', twitter: 'hello')
+      @user2 = User.create!(first_name:'Susann', last_name:'Mansoorii', email:'example2@gmail.com', password: 'meow123', instagram: 'goodbye')
+     
     end
-  end
+
+    it "deletes a user from the DB" do
+      expect{delete :destroy, id: @user2.id}.to change(User, :count).by(-1)
+    end
+
+  # it "dd" do 
+   #   expect(@user1).to eq(nil)
+   #end
+    
+  end #End destroy controller tests
+
 
 end
--- spec/mailers/recommendation_mailer_spec.rb
+++ spec/mailers/recommendation_mailer_spec.rb
@@ -3,16 +3,6 @@ require "rails_helper"
 RSpec.describe RecommendationMailer, type: :mailer do
   describe "new_recommendation" do
     let(:mail) { RecommendationMailer.new_recommendation }
-
-    it "renders the headers" do
-      expect(mail.subject).to eq("New recommendation")
-      expect(mail.to).to eq(["to@example.org"])
-      expect(mail.from).to eq(["from@example.com"])
-    end
-
-    it "renders the body" do
-      expect(mail.body.encoded).to match("Hi")
-    end
-  end
+end
 
 end
