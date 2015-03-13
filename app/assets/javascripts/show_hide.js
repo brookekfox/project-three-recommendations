5730871457ea9421ccdba695c4c83f37c305797a
Marc Cruz <fcpmarcus@gmail.com>
2015-03-13 14:53:01 -0700

    edited public and fix hidden rec option

--- app/assets/javascripts/show_hide.js
+++ app/assets/javascripts/show_hide.js
@@ -4,10 +4,4 @@ $(document).ready(function () {
 		$('.private_recommendation').toggle(this.checked);
 	}).change();
 
-	//if ($('.SessionValue').val() == session['user_id']) {
-	//	$('.actions-for-current-user').show();
-	//} else {
-	//	$('.actions-for-current-user').hide();
-	//}
-
 });
